2d4a6f8f768cd2beca3fbb787df553ba522e596d
liulei435 <leiu2520@126.com>
2019-08-18 18:49:10 +0800

    add:添加状态模式

---
+++ src/com/leiu2520/TemplateMethod/Main.java
@@ -0,0 +1,33 @@
+package com.leiu2520.TemplateMethod;
+
+/**
+ *  模板方法就是钩子函数
+ */
+public class Main {
+    public static void main(String[] args) {
+        F f = new C1();
+        f.m();
+    }
+}
+abstract class F{
+    public void m(){
+        op1();
+        op2();
+    }
+
+    abstract void op1();
+    abstract void op2();
+}
+
+class C1 extends F{
+
+    @Override
+    void op1() {
+        System.out.println("op1");
+    }
+
+    @Override
+    void op2() {
+        System.out.println("op2");
+    }
+}
---
+++ src/com/leiu2520/state/Main.java
@@ -0,0 +1,24 @@
+package com.leiu2520.state;
+
+/**
+ * 状态模式
+ *
+ */
+public class Main {
+    String name;
+    private enum MMState {HAPPY,SAD};
+
+    MMState state;
+
+    public void smile(){
+
+    }
+
+    public void cry(){
+
+    }
+
+    public void say(){
+
+    }
+}
